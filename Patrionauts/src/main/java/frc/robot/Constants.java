ccaa6002bf8a4929840e8126d8c497f60163bae7
Alonzo F <alonzof500@gmail.com>
2020-01-07 09:19:12 -0800

    Used sample code to attempt to get a motor moving

--- Patrionauts/src/main/java/frc/robot/Constants.java
+++ Patrionauts/src/main/java/frc/robot/Constants.java
@@ -9,4 +9,11 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static final int JOYSTICK_1 = 0;
+    
+	public static final int LEFT_MOTOR_1 = 0;
+	public static final int LEFT_MOTOR_2 = 1;
+	public static final int RIGHT_MOTOR_1 = 2;
+	public static final int RIGHT_MOTOR_2 = 3;
 }
