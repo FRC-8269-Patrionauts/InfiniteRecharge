115d72fa14d538bd87ebf50cbf9a728667e08c48
FRC8269 <58281403+FRC8269@users.noreply.github.com>
2020-01-18 13:32:38 -0800

    ramp up motor speed for drive system
    turning and basic movement

--- Patrionauts/src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ Patrionauts/src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CameraSubsystem extends SubsystemBase {
--- Patrionauts/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ Patrionauts/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -45,149 +45,136 @@ public class DriveSubsystem extends SubsystemBase {
   private final SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightMotor1, rightMotor2);
 
   private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
-  
-  private double goalSpeedlM1 = 0;
-  private double goalSpeedlM2 = 0;
-  private double goalSpeedrM1 = 0;
-  private double goalSpeedrM2 = 0;
 
-  private double currentSpeedlM1 = 0;
-  private double currentSpeedlM2 = 0;
-  private double currentSpeedrM1 = 0;
-  private double currentSpeedrM2 = 0;
-  
+  private double goalSpeedx = 0;
+  private double goalSpeedz = 0;
+
+  private double currentSpeedx = 0;
+  private double currentSpeedz = 0;
+ 
+  private static final double SPEED_STEP_UP = 0.04;
+  private static final double SPEED_STEP_DOWN = 0.05;
+
   double speedMult = 1;
-  public DriveSubsystem(){
+
+  public DriveSubsystem() {
+   // drive.setDeadband(0);
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    //leftMotor1
-    if (goalSpeedlM1 > currentSpeedlM1){
-      currentSpeedlM1 += .02;
-    }else if (goalSpeedlM1 < currentSpeedlM1){
-      currentSpeedlM1 -= .02;
+    // leftMotor1
+    if (goalSpeedx > currentSpeedx) {
+      currentSpeedx += SPEED_STEP_UP;
+    } else if (goalSpeedx < currentSpeedx) {
+      currentSpeedx -= SPEED_STEP_DOWN;
     }
-     if (currentSpeedlM1 > 1){
-       currentSpeedlM1 = 1;
-     }else if (currentSpeedlM1 < -1){
-       currentSpeedlM1 = -1;
-     }
-    leftMotor1.set(currentSpeedlM1);
+    if (currentSpeedx > 1) {
+      currentSpeedx = 1;
+    } else if (currentSpeedx < -1) {
+      currentSpeedx = -1;
+    }
+    if (Math.abs(currentSpeedx) > 1){
+      currentSpeedx = 1;
+    }
 
-    //leftMotor2
-    if (goalSpeedlM2 > currentSpeedlM2){
-      currentSpeedlM2 += .02;
-    }else if (goalSpeedlM2 < currentSpeedlM2){
-      currentSpeedlM2 -= .02;
+    if (goalSpeedz > currentSpeedz) {
+      currentSpeedz += SPEED_STEP_UP;
+    } else if (goalSpeedz < currentSpeedz) {
+      currentSpeedz -= SPEED_STEP_DOWN;
     }
-    if (currentSpeedlM2 > 1){
-      currentSpeedlM2 = 1;
-    }else if (currentSpeedlM2 < -1){
-      currentSpeedlM2 = -1;
+    if (currentSpeedz > 1) {
+      currentSpeedz = 1;
+    } else if (currentSpeedz < -1) {
+      currentSpeedz = -1;
     }
-    leftMotor2.set(currentSpeedlM2);
 
-    //rightMotor1
-    if (goalSpeedrM1 > currentSpeedrM1){
-      currentSpeedrM1 += .02;
-    }else if (goalSpeedrM1 < currentSpeedrM1){
-      currentSpeedrM1 -= .02;
-    }
-    if (currentSpeedrM1 > 1){
-      currentSpeedrM1 = 1;
-    }else if (currentSpeedrM1 < -1){
-      currentSpeedrM1 = -1;
-    }
-    rightMotor1.set(currentSpeedrM1);
+    if (Math.abs(currentSpeedx) > 0.05 || Math.abs(currentSpeedz) > 0.05){
+      drive.arcadeDrive(currentSpeedx, currentSpeedz);
+    } 
+      //Update to currentSpeedX and Z
 
-    //rightMotor2
-    if (goalSpeedrM2 > currentSpeedrM2){
-      currentSpeedrM2 += .02;
-    }else if (goalSpeedrM2 < currentSpeedrM2){
-      currentSpeedrM2 -= .02;
-    }
-    if (currentSpeedrM2 > 1){
-      currentSpeedrM2 = 1;
-    }else if (currentSpeedrM2 < -1){
-      currentSpeedrM2 = -1;
-    }
-    rightMotor2.set(currentSpeedrM2);
   }
+
   public void setBase(double lM1, double lM2, double rM1, double rM2) {
-    goalSpeedlM1 = lM1;
-    goalSpeedlM2 = lM2;
-    goalSpeedrM1 = -rM1;
-    goalSpeedrM2 = -rM2;
-    //leftMotor1.set(lM1*speedMult);
-    //leftMotor2.set(lM2*speedMult);
-    //rightMotor1.set(rM1*speedMult);
-    //rightMotor2.set(rM2*speedMult);
+    // goalSpeedlM1 = lM1;
+    // goalSpeedlM2 = lM2;
+    // goalSpeedrM1 = -rM1;
+    // goalSpeedrM2 = -rM2;
+    // leftMotor1.set(lM1*speedMult);
+    // leftMotor2.set(lM2*speedMult);
+    // rightMotor1.set(rM1*speedMult);
+    // rightMotor2.set(rM2*speedMult);
   }
 
-  public void arcadeDrive(double x, double z){ 
-    //drive.arcadeDrive(x * speedMult, z * speedMult);
-    goalSpeedlM1 = x;
-    goalSpeedlM2 = x;
-    goalSpeedrM1 = -x;
-    goalSpeedrM2 = -x;
+  public void arcadeDrive(double x, double z) {
+    goalSpeedx = x;
+    goalSpeedz = z;
   }
 
-  public void strafe(double x) {   //please for the love of all that is holy stafe with the grace of our permethious
+  public void strafe(double x) { // please for the love of all that is holy strafe with the grace of our
+                                 // permethious
     double xValue = x;
-    if (Math.abs(xValue) < .5){
+    if (Math.abs(xValue) < .5) {
       xValue = 0;
     }
-      setBase(x, -x, x, -x);
-  }
-  public void setBaseAll(double speed){
-    setBase(-speed,-speed,speed,speed);
+    setBase(x, -x, x, -x);
   }
 
-  public void spin180(double speed){
+  public void setBaseAll(double speed) {
+    setBase(-speed, -speed, speed, speed);
+  }
+
+  public void spin180(double speed) {
     // we need encoders
   }
-  
+
   public void rotation(double speed) {
-    setBase(-speed,-speed,-speed,-speed);
+    setBase(-speed, -speed, -speed, -speed);
   }
 
   public void stop() {
-    setBase(0, 0, 0, 0);
+    arcadeDrive(0, 0);
   }
 
-  public void Limit(){//idk man
-    setBase(.2,.2,.2,.2);
+  public void Limit() {// idk man
+    setBase(.2, .2, .2, .2);
   }
 
-  public void setSpeed(double speed){
+  public void setSpeed(double speed) {
     this.speedMult = speed;
   }
   // autonnomous thingies
 
-
-  public SpeedController getLeftMotor1(){
+  public SpeedController getLeftMotor1() {
     return leftMotor1;
   }
-  public SpeedController getLeftMotor2(){
+
+  public SpeedController getLeftMotor2() {
     return leftMotor2;
   }
-  public SpeedController getRightMotor1(){
+
+  public SpeedController getRightMotor1() {
     return rightMotor1;
   }
-  public SpeedController getRightMotor2(){
+
+  public SpeedController getRightMotor2() {
     return rightMotor2;
   }
 
   public double getLeftMotor1Speed() {
     return leftMotor1.get();
   }
+
   public double getLeftMotor2Speed() {
     return leftMotor2.get();
   }
+
   public double getRightMotor1Speed() {
     return rightMotor1.get();
   }
+
   public double getRightMotor2Speed() {
     return rightMotor2.get();
   }
